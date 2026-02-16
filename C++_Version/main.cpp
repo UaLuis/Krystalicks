5631c66bb6b1d06f98d8d843d910499bec07e6f8
UaLuis <ua.luis01@gmail.com>
2026-02-16 08:46:43 +0200

    C++

---
+++ C++_Version/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//int myOld = 15;
+//string myName = "Kristian";
+string txt = "var Start 10";
+
+int main() {
+    //const int x = 0;
+    //cout << myName << "\n"; //Print
+    //cout << myOld << "\n";
+    //cin >> myOS; //Input
+    //auto x = 2;
+    //cout << ++x; --x, += -= і т.д
+    //string Linux = "Hello ";
+    //string NewLinux = Linux.append("Linux!");
+    //cout << NewLinux;
+    
+    cout << txt.length();
+
+
+    return 0; //Laways Need!
+}
